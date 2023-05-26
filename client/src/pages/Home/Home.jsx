6f686d8615f70b1af84a09721cecd2cd4b51e1ef
khardy <khardy.official@gmail.com>
2023-05-26 12:25:18 +0300

    added backend and database

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,7 +2,19 @@ import './Home.scss';
 import MenuList from '../../Containers/MenuList/MenuList';
 import Sidebar from '../../Containers/Sidebar/Sidebar';
 
+import { useEffect } from 'react';
+import axios from '../../axios';
+
 const Home = () => {
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/products');
+            console.log(data);
+        }
+        fetchProducts();
+
+    }, []);
 
     return (
         <main className="Home">
---
+++ server/index.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import ProductModel from './models/Product.js';
+import OrderModel from './models/Order.js';
+
+mongoose.connect('mongodb+srv://admin:1q2w3e@cluster0.estzqnq.mongodb.net/delivery?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/products', async (req, res) => {
+
+    try {
+        const products = await ProductModel.find().sort({ discount: -1 });
+
+        if (!products) {
+            return res.status(404).json({
+                message: 'Error! Products not found.'
+            })
+        }
+
+        res.json(products);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Connection error.'
+        })
+    }
+});
+
+app.post('/orders', async (req, res) => {
+    try {
+        const doc = new OrderModel(req.body);
+        const order = await doc.save();
+        res.json(order);
+    } catch (error) {
+        res.json({
+            message: 'failed to add order',
+            error,
+        })
+    }
+})
+
+app.listen(8181, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log('Server ok');
+});
